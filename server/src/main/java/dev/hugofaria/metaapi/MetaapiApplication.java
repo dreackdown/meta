bfe853ae11764664cbf4ebed3e9c8b928597ac65
dreackdown <hugofaria157@live.com>
2023-08-18 01:50:24 -0300

    atualização spring boot 3

---
+++ server/src/main/java/dev/hugofaria/metaapi/MetaapiApplication.java
@@ -0,0 +1,12 @@
+package dev.hugofaria.metaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaapiApplication.class, args);
+    }
+}
